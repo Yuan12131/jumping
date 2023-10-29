8d6ea14e4a6935a568fc9fc59a6abf98aa40d662
leeyuan <dbdks32123@gmail.com>
2023-10-29 18:16:08 +0900

    포트번호, 미들웨어 설정

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+
+const port = 8019; // 사용할 포트 번호
+
+// 정적 파일을 제공하기 위한 미들웨어 설정
+app.use(express.static('static'));
